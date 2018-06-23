e4d5b133b53a8eb3d62d6705790f68470f6572f4
Marek Bućko <buckomarek1@gmail.com>
2018-06-23 14:10:01 +0200

    test1

--- src/main/java/pl/sda/todolist/controller/ToDoListController.java
+++ src/main/java/pl/sda/todolist/controller/ToDoListController.java
@@ -7,23 +7,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.todolist.model.ToDoTask;
+import pl.sda.todolist.repository.ToDoRepository;
 import pl.sda.todolist.service.ToDoListService;
 
 import java.util.List;
 
 
-    @RestController
-    @RequestMapping("/todolist")
-    public class ToDoListController {
+@RestController
+@RequestMapping("/todolist")
+public class ToDoListController {
 
-        @Autowired
-        private ToDoListService toDoListService;
+    @Autowired
+    private ToDoListService toDoListService;
+
+    @Autowired
+    private ToDoRepository toDoRepository;
 
 
-        @GetMapping
-        @ResponseStatus(HttpStatus.OK)
-        public List<ToDoTask> getAll (){
-            return  toDoListService.search();
-        }
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ToDoTask> getAll() {
+        return toDoRepository.findAll();
     }
+}
 
--- src/main/java/pl/sda/todolist/repository/ToDoRepository.java
+++ src/main/java/pl/sda/todolist/repository/ToDoRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.sda.todolist.model.ToDoTask;
 
+import java.util.ArrayList;
+
 @Repository
 public interface ToDoRepository extends JpaRepository<ToDoTask, Long> {
+
 }
